12da0e4d5f09f8a71b2ca7bc78a491ac0d697921
manthanshirke23-bit <manthanshirke23@gmail.com>
2026-02-21 13:09:11 +0530

    first commit - tic tac toe game

---
+++ script.js
@@ -0,0 +1,61 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("status");
+
+let currentPlayer = "X";
+let gameActive = true;
+
+const winPatterns = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
+
+cells.forEach((cell, index) => {
+  cell.addEventListener("click", () => cellClick(cell, index));
+});
+
+function cellClick(cell, index) {
+
+  if (cell.textContent !== "" || !gameActive) return;
+
+  cell.textContent = currentPlayer;
+
+  if (checkWinner()) {
+    statusText.textContent = `Player ${currentPlayer} Wins! 🎉`;
+    gameActive = false;
+    return;
+  }
+
+  if (isDraw()) {
+    statusText.textContent = "Game Draw 😎";
+    gameActive = false;
+    return;
+  }
+
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  statusText.textContent = `Player ${currentPlayer} Turn`;
+}
+
+function checkWinner() {
+  return winPatterns.some(pattern => {
+    return pattern.every(index => {
+      return cells[index].textContent === currentPlayer;
+    });
+  });
+}
+
+function isDraw() {
+  return [...cells].every(cell => cell.textContent !== "");
+}
+
+function restartGame() {
+  cells.forEach(cell => cell.textContent = "");
+  currentPlayer = "X";
+  gameActive = true;
+  statusText.textContent = "Player X Turn";
+}
